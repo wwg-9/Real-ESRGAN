288bc2d940408a9a48a902494d0213bac5012693
Mamank <adullawi919@gmail.com>
2026-02-20 10:43:58 +0700

    Update model.py

--- RealESRGAN/model.py
+++ RealESRGAN/model.py
@@ -4,25 +4,28 @@ from torch.nn import functional as F
 from PIL import Image
 import numpy as np
 import cv2
-from huggingface_hub import hf_hub_url, cached_download
+from huggingface_hub import hf_hub_url, hf_hub_download
 
 from .rrdbnet_arch import RRDBNet
 from .utils import pad_reflect, split_image_into_overlapping_patches, stich_together, \
-                   unpad_image
+                  unpad_image
 
 
 HF_MODELS = {
     2: dict(
         repo_id='sberbank-ai/Real-ESRGAN',
         filename='RealESRGAN_x2.pth',
+        repo_type='model',
     ),
     4: dict(
         repo_id='sberbank-ai/Real-ESRGAN',
         filename='RealESRGAN_x4.pth',
+        repo_type='model',
     ),
     8: dict(
         repo_id='sberbank-ai/Real-ESRGAN',
         filename='RealESRGAN_x8.pth',
+        repo_type='model',
     ),
 }
 
@@ -32,20 +35,20 @@ class RealESRGAN:
         self.device = device
         self.scale = scale
         self.model = RRDBNet(
-            num_in_ch=3, num_out_ch=3, num_feat=64, 
+            num_in_ch=3, num_out_ch=3, num_feat=64,
             num_block=23, num_grow_ch=32, scale=scale
         )
-        
+
     def load_weights(self, model_path, download=True):
         if not os.path.exists(model_path) and download:
             assert self.scale in [2,4,8], 'You can download models only with scales: 2, 4, 8'
             config = HF_MODELS[self.scale]
             cache_dir = os.path.dirname(model_path)
             local_filename = os.path.basename(model_path)
-            config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
-            cached_download(config_file_url, cache_dir=cache_dir, force_filename=local_filename)
+            hf_hub_download(repo_id=config['repo_id'], filename=config['filename'], repo_type=config['repo_type'], cache_dir=cache_dir, force_filename=local_filename)
             print('Weights downloaded to:', os.path.join(cache_dir, local_filename))
-        
+
+
         loadnet = torch.load(model_path)
         if 'params' in loadnet:
             self.model.load_state_dict(loadnet['params'], strict=True)
@@ -55,8 +58,8 @@ class RealESRGAN:
             self.model.load_state_dict(loadnet, strict=True)
         self.model.eval()
         self.model.to(self.device)
-        
-    @torch.cuda.amp.autocast()
+
+    @torch.amp.autocast('cuda')
     def predict(self, lr_image, batch_size=4, patches_size=192,
                 padding=24, pad_size=15):
         scale = self.scale
@@ -80,7 +83,7 @@ class RealESRGAN:
         padded_size_scaled = tuple(np.multiply(p_shape[0:2], scale)) + (3,)
         scaled_image_shape = tuple(np.multiply(lr_image.shape[0:2], scale)) + (3,)
         np_sr_image = stich_together(
-            np_sr_image, padded_image_shape=padded_size_scaled, 
+            np_sr_image, padded_image_shape=padded_size_scaled,
             target_shape=scaled_image_shape, padding_size=padding * scale
         )
         sr_img = (np_sr_image*255).astype(np.uint8)
